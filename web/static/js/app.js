c34a030043d89961f5043e1161cc3574d574f8a7
Dave Lively <davelively14@gmail.com>
2016-07-07 21:21:47 -0400

    Basic Elm configured

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,7 +12,7 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import Elm from "./main";
+import Elm from './main';
 const elmDiv = document.querySelector('#elm-target');
 if (elmDiv) {
   Elm.Main.embed(elmDiv);
